907a825ab3dfb34cab0e384bbe21034f0ca4a813
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-08 18:32:33 +0900

    :sparkles: transition

--- src/router.svelte
+++ src/router.svelte
@@ -1,18 +1,22 @@
 <script>
     import { Route } from 'tinro';
-    import About from './pages/About.svelte';
-    import Home from './pages/Home.svelte';
-    import Contacts from './pages/Contacts.svelte';
+    import About from './components/About.svelte';
+    import Home from './components/Home.svelte';
+    import Contacts from './components/Contacts.svelte';
     // import SubContent from './pages/SubContent.svelte';
+    import Transition from './Transition.svelte';
 </script>
 
-<Route path="/" redirect="/home"><Home /></Route>
-<Route path="/home"><Home /></Route>
-<Route path="/about/*">
-    <!-- 반드시 *을 사용해야함 (router.svelte에서 띄우든, 상위 page에 component로 포함시키든) -->
-    <About />
-    <!-- <Route path="/sub">
-        <SubContent />
-    </Route> -->
-</Route>
-<Route path="/contacts"><Contacts /></Route>
+<Transition>
+    <Route path="/" redirect="/home"><Home /></Route>
+    <Route path="/home"><Home /></Route>
+    <Route path="/about/*">
+        <!-- 반드시 *을 사용해야함 (router.svelte에서 띄우든, 상위 page에 component로 포함시키든) -->
+        <About />
+        <!-- <Route path="/sub">
+            <SubContent />
+        </Route> -->
+    </Route>
+    <Route path="/contacts"><Contacts /></Route>
+    <Route fallback>No page found</Route>
+</Transition>
