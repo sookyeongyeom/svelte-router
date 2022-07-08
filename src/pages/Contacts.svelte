5c6a36c2584523ecea92c0e8311eaa07ac02b13a
sookyeongyeom <sookyeong.dev@gmail.com>
2022-07-08 18:24:49 +0900

    :sparkles: router

--- src/pages/Contacts.svelte
+++ src/pages/Contacts.svelte
@@ -1,1 +1,23 @@
+<script>
+    import { router } from 'tinro';
+
+    const goHome = () => router.goto('home');
+</script>
+
 <h1>Contacts Page</h1>
+<p class="on" on:click={goHome}>Go Home!</p>
+
+<!--
+router.goto('/foo'); // URL: /foo
+router.location.query.set('name', 'alex') // URL: /foo?name=alex
+router.location.hash.set('bar') // URL: /foo?name=alex#bar
+router.location.query.set('page', '1') // URL: /foo?name=alex#page=1#bar
+router.location.query.replace({hello: 'world'}) // URL: /foo?hello=world#bar
+router.location.query.clear() // URL: /foo#bar
+router.location.hash.clear() // URL: /foo
+-->
+<style>
+    .on {
+        cursor: pointer;
+    }
+</style>
